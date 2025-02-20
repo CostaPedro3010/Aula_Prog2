b05915747ec519fb71481a1cd70dd64fa614e8ef
Pedro Costa <costapedro3010@gmail.com>
2025-02-20 19:37:58 -0300

    PRIMEIRA AULA ASSISTIDA

---
+++ Aula_01_python/Dicionarios.py
@@ -0,0 +1,18 @@
+Lista = []
+dicionario = {}
+
+print(type(dicionario))
+
+dicionario['Porto Alegre'] = "1500000"
+dicionario['Alegrete'] = "75000"
+dicionario['Pelotas'] = "340000"
+dicionario['Caxias'] = "480000"
+dicionario['Canoas'] = "360000"
+dicionario['Gravatai'] = "280000"
+dicionario['Cachoeirinha'] = "141000"
+dicionario['guaiba'] = "95000"
+dicionario['Santa Maria'] = "280000"
+dicionario['Gramado'] = "70000"
+dicionario['Canela'] = "70000"
+
+print(dicionario)
