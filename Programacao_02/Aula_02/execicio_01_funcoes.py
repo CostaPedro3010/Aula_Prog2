9a3be2d2523bb1e3c910e0530444b32635be5efc
Pedro Costa <costapedro3010@gmail.com>
2025-03-13 22:24:15 -0300

    Preparacao para exercicio

--- Programacao_02/Aula_02/execicio_01_funcoes.py
+++ Programacao_02/Aula_02/execicio_01_funcoes.py
@@ -1,5 +1,17 @@
+print(" ")
 print("Exercicio 01: ")
 def nomeSobrenome(nome,sobrenome):
     nomeCompleto = nome+sobrenome
     print(nomeCompleto)
 nomeSobrenome("Pedro ", "Costa")
+
+print(" ")
+print("Exercicio 02: ")
+def somaDoisNros(primeiroArgumento, segundoArgumento):
+    soma2nros = primeiroArgumento + segundoArgumento
+    return soma2nros
+print("Soma entre dois argumentos: ",somaDoisNros(1,3))
+
+print(" ")
+print("Exercicio 03: ")
+
